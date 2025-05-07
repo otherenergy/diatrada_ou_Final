dc24a6717c16eca4e096a062294bfad35e4ea66a
otherenergy <otherenergy@hotmail.com>
2025-05-07 20:56:25 +0200

    Data Maps Updated 1

--- src/components/Creative/Contact/Map.jsx
+++ src/components/Creative/Contact/Map.jsx
@@ -2,7 +2,7 @@
 const Map = () => {
   return (
     <div className="google-maps mt-100">
-      <iframe id="gmap_canvas" src="https://maps.google.com/maps?q=hollwood&t=&z=11&ie=UTF8&iwloc=&output=embed"></iframe>
+      <iframe id="gmap_canvas" src="https://maps.google.com/maps?width=100%25&amp;height=200&amp;hl=en&amp;q=Kesklinna%20linnaosa,%20Jarvevana%20tee%209,%2011314,%20Tallinn,%20Estonia+(DiatradaOU)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
     </div>
   )
 }
