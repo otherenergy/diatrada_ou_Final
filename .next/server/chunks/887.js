02d8360aa337a6636b990cbe8eae2e74936806a4
otherenergy <otherenergy@hotmail.com>
2025-05-17 17:00:31 +0200

    Message blue sent

---
+++ .next/server/chunks/887.js
@@ -0,0 +1,49 @@
+"use strict";
+exports.id = 887;
+exports.ids = [887];
+exports.modules = {
+
+/***/ 8028:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+
+
+class Split extends (react__WEBPACK_IMPORTED_MODULE_1___default().Component) {
+    target = /*#__PURE__*/ react__WEBPACK_IMPORTED_MODULE_1___default().createRef();
+    split = ()=>{
+        if (this.target.current) {
+            Splitting({
+                target: this.target.current
+            });
+        }
+    };
+    componentDidMount = this.split;
+    componentDidUpdate = this.split;
+    render() {
+        return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+            ref: this.target,
+            children: this.props.children
+        });
+    }
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Split);
+
+
+/***/ }),
+
+/***/ 2012:
+/***/ ((module) => {
+
+module.exports = JSON.parse('[{"id":1,"title":"Cloud Innovative Solutions","details":"Innovative Solutions that help you understand the intricacies of the crypto world.","closed":false},{"id":2,"title":"Solar Top Cloud Tools","details":"Blockchain Top Cloud Tools that help you utilize leading cloud platforms to optimize your solar energy projects.","closed":true},{"id":3,"title":"Blockchain Expert Consulting","details":"Expert Consulting in this future changing of seasoned professionals to offers you 24/7 full consulting support.","closed":true}]');
+
+/***/ })
+
+};
+;
